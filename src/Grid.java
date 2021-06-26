37d78ebc8e4fd06fbb21752b8981a3607130981f
Mike <msthygesen16@gmail.com>
2021-06-26 18:10:47 +1000

    added clear grid feature

--- src/Grid.java
+++ src/Grid.java
@@ -2,25 +2,28 @@ package src;
 
 import java.awt.*;
 
-
 public class Grid {
     
+
+    // specify how many rows and columns
     int rows = 10;
     int columns = 10;
 
+    // create a 2d of cells that is of size rows * columns
     Cell[][] cells = new Cell[rows][columns];
 
-
+    // default constructor 
     public Grid(){
-
         for (int i = 0; i < rows; i++){
             for (int j = 0; j < columns; j++){
                 cells[i][j] = new Cell(i,j);
+                
             }
         }
 
     }
 
+    // painting the grid of squares
     public void paint(Graphics g, Point mousePos){
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < columns; j++){
@@ -30,6 +33,23 @@ public class Grid {
 
     }
 
+    // all cells have their visited boolean set to false
+    public void resetGrid(){
+        for (int r = 0; r < rows; r++){
+            for (int c = 0; c < columns; c++){
+                cells[r][c].visited = false;
+            }
+        }
+    }
 
+    // does the inverse of reset
+    public void fillGrid(){
+        for (int r = 0; r < rows; r++){
+            for (int c = 0; c < columns; c++){
+                cells[r][c].visited = true;
+            }
+        }
+    }
+
+   
 }
-
--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,82 @@
 package src;
 
 import javax.swing.*;
+
 import java.awt.*;
+import java.awt.event.*;
+
 
 class Main extends JFrame {
     
 
-    class App extends JPanel {
+    class App extends JPanel implements KeyListener {
         
         Grid grid;
+        
 
+        // here we set size of the window
+        // and create a new grid object
         public App() {
+            // attach key listener to App object
+            addKeyListener(this); // for resetting grid
             setPreferredSize(new Dimension(500, 500));
             grid = new Grid();
+            
         }
 
         @Override
         public void paint(Graphics g) {
-            
+            // paint grid object we created
             grid.paint(g, getMousePosition());
         }
 
-   
+        // used for detecting when a key is pressed
+        @Override
+        public void keyPressed(KeyEvent e) {
+            
+            int key = e.getKeyCode();
+
+            // R key restarts the app
+            if (key == KeyEvent.VK_R){
+                grid.resetGrid();
+            }
+
+            // space fills the grid
+            if (key == KeyEvent.VK_SPACE){
+                grid.fillGrid();
+            }
+            
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            
+        }
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+
+            
+        }
+
 
     }
-
 
     public static void main(String[] args) throws Exception {
         Main window = new Main();
         window.run();
+
     }
 
     private Main() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         App canvas = new App();
+        // attach keylistener to canvas (which is an App object)
+        this.addKeyListener(canvas); // for ressting grid
         this.setContentPane(canvas);
         this.pack();
         this.setVisible(true);
-        this.setTitle("Test App");
+        this.setTitle("Java Noise Project"); // set title of app
     }
 
     public void run() {
@@ -45,5 +84,6 @@ class Main extends JFrame {
             this.repaint();
         }
     }
+
 
 }
